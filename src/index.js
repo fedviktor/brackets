b7607a095356cefba2f7ff47397020ab9335a9f8
fedviktor <https://github.com/fedviktor>
2020-10-25 10:02:54 +0200

    fix: src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str==='()'){return true}
+     else if(str==='()'&&srt==='[]' || str==='||'){return true}
+     else if( str=')(' || str==='({)}' || str==='|(|)'){return false}
+    
+  return bracketsConfig
+
 }
